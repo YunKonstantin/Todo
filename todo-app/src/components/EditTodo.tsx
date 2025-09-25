9ca35087198aee87044c68ac94ef466a3ca61b4e
Konstantin <yunkonstantin@icloud.com>
2025-09-25 12:10:49 +0300

    dsa

--- todo-app/src/components/EditTodo.tsx
+++ todo-app/src/components/EditTodo.tsx
@@ -14,8 +14,8 @@ const EditTodo = ({ initialText, onSave, onCancel }: EditTodoProps) => {
   };
 
   return (
-    <div style={{ display: "flex", gap: 8 }}>
-      <input value={value} onChange={(e) => setValue(e.target.value)} style={{ flex: 1, padding: 6 }} />
+    <div style={{ display: "flex", gap: 8,  backgroundColor: "blue" }}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} style={{ flex: 1, padding: 6, backgroundColor:"blue" }} />
       <button onClick={handleSave}>Сохранить</button>
       <button onClick={onCancel}>Отмена</button>
     </div>
