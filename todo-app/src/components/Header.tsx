1b0c542f322ad3b3b2f38110490983016d8c3ebc
Konstantin <yunkonstantin@icloud.com>
2025-10-30 00:48:10 +0300

    123

--- todo-app/src/components/Header.tsx
+++ todo-app/src/components/Header.tsx
@@ -1,14 +1,13 @@
-
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import type { AppDispatch, RootState } from '../store';
-import { fetchUserProfile, logoutUser } from '../store/slices/authSlice';
-import { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import type { AppDispatch, RootState } from "../store";
+import { fetchUserProfile, logoutUser } from "../store/slices/authSlice";
+import { useEffect } from "react";
 
 export const Header = () => {
   const { token, user } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch<AppDispatch>();
-   useEffect(() => {
+  useEffect(() => {
     if (token && !user) {
       dispatch(fetchUserProfile());
     }
@@ -16,30 +15,34 @@ export const Header = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     dispatch(logoutUser());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <header className="header">
       <div className="header-container">
-        <Link to="/" className="logo">
-          Менеджер задач
-        </Link>
-        
         <nav className="nav">
           {token ? (
             <>
               <span className="user-email">Привет, {user?.email}</span>
-              <Link to="/" className="nav-link">Задачи</Link>
-              <Link to="/profile" className="nav-link">Профиль</Link>
+              <Link to="/" className="nav-link">
+                Задачи
+              </Link>
+              <Link to="/profile" className="nav-link">
+                Профиль
+              </Link>
               <button onClick={handleLogout} className="nav-button">
                 Выйти
               </button>
             </>
           ) : (
             <>
-              <Link to="/login" className="nav-link">Войти</Link>
-              <Link to="/register" className="nav-link">Регистрация</Link>
+              <Link to="/login" className="nav-link">
+                Войти
+              </Link>
+              <Link to="/register" className="nav-link">
+                Регистрация
+              </Link>
             </>
           )}
         </nav>
